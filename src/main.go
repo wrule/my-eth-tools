0927b8af8fc50a37f887dd445b9cd956ab1c7e8a
jimao <1982775886@qq.com>
2021-01-05 11:14:49 +0800

    私钥生成

--- src/main.go
+++ src/main.go
@@ -1,25 +1,8 @@
 package main
 
-import (
-	"fmt"
-	"os"
-)
+import "fmt"
 
 func main() {
-	file, err := os.Open("/dev/random")
-	if err != nil {
-		panic("打开文件出错")
-	}
-	buf := make([]byte, 32)
-	n, err := file.Read(buf)
-	if err != nil {
-		panic("读取文件出错")
-	}
-	fmt.Println(n)
-	for _, num := range buf {
-		fmt.Printf("%02x ", num)
-	}
-	fmt.Println()
-	file.Close()
-	fmt.Println("你好，世界")
+	key := NewPriKey()
+	fmt.Println(key.Hex())
 }
--- src/prikey.go
+++ src/prikey.go
@@ -1,4 +1,41 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 // PriKey 私钥
 type PriKey [32]byte
+
+// Bytes 获取字节切片
+func (me PriKey) Bytes() []byte {
+	return me[:]
+}
+
+// Hex 获取十六进制私钥字符串
+func (me PriKey) Hex() string {
+	rst := ""
+	for _, num := range me.Bytes() {
+		rst += fmt.Sprintf("%02x", num)
+	}
+	return rst
+}
+
+// NewPriKey 构造函数
+func NewPriKey() PriKey {
+	const size = 32
+	file, err := os.Open("/dev/random")
+	if err != nil {
+		panic("随机文件打开出错")
+	}
+	buf := make([]byte, size)
+	n, err := file.ReadAt(buf, size)
+	if err != nil || n != size {
+		panic("随机文件读取出错")
+	}
+	file.Close()
+	rst := [size]byte{}
+	copy(rst[:], buf)
+	return PriKey(rst)
+}
