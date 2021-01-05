2196ccb151c0a12f809b8a60bece7081c3b1d9bd
jimao <1982775886@qq.com>
2021-01-06 00:41:55 +0800

    测试一半

--- src/main.go
+++ src/main.go
@@ -4,14 +4,28 @@ import (
 	"encoding/hex"
 	"fmt"
 
-	ethCrypto "github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/crypto"
 )
 
 func main() {
-	bytes, err := hex.DecodeString("6e145ccef1033dea239875dd00dfb4fee6e3348b84985c92f103444683bae07b83b5c38e5e2b0c8529d7fa3f64d46daa1ece2d9ac14cab9477d042c84c32ccd0")
+	// bytes, err := hex.DecodeString("6e145ccef1033dea239875dd00dfb4fee6e3348b84985c92f103444683bae07b83b5c38e5e2b0c8529d7fa3f64d46daa1ece2d9ac14cab9477d042c84c32ccd0")
+	// if err != nil {
+	// 	panic("")
+	// }
+
+	pubKeyHex := "f8f8a2f43c8376ccb0871305060d7b27b0554d2cc72bccf41b2705608452f315"
+	pubKeyBytes, err := hex.DecodeString(pubKeyHex)
 	if err != nil {
 		panic("")
 	}
-	rst := ethCrypto.Keccak256Hash(bytes)
-	fmt.Println(rst)
+	for _, num := range pubKeyBytes {
+		fmt.Printf("%02x ", num)
+	}
+	fmt.Println()
+	fmt.Println(len(pubKeyBytes))
+	fmt.Println(hex.EncodeToString(pubKeyBytes))
+	// rst := crypto.Keccak256Hash(bytes)
+	// fmt.Println(rst)
+	sig := make([]byte, crypto.SignatureLength)
+	fmt.Println(crypto.Ecrecover(pubKeyBytes, sig))
 }
