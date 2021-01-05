7d17e335997d3f204b813262b52633de3f8dd325
jimao <1982775886@qq.com>
2021-01-06 01:38:28 +0800

    椭圆曲线加密

--- src/main.go
+++ src/main.go
@@ -1,31 +1,24 @@
 package main
 
 import (
+	"crypto/elliptic"
 	"encoding/hex"
 	"fmt"
 
 	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/crypto/ecies"
 )
 
-func main() {
-	// bytes, err := hex.DecodeString("6e145ccef1033dea239875dd00dfb4fee6e3348b84985c92f103444683bae07b83b5c38e5e2b0c8529d7fa3f64d46daa1ece2d9ac14cab9477d042c84c32ccd0")
-	// if err != nil {
-	// 	panic("")
-	// }
-
-	pubKeyHex := "f8f8a2f43c8376ccb0871305060d7b27b0554d2cc72bccf41b2705608452f315"
-	pubKeyBytes, err := hex.DecodeString(pubKeyHex)
+func hexKey(prv string) *ecies.PrivateKey {
+	key, err := crypto.HexToECDSA(prv)
 	if err != nil {
-		panic("")
+		panic(err)
 	}
-	for _, num := range pubKeyBytes {
-		fmt.Printf("%02x ", num)
-	}
-	fmt.Println()
-	fmt.Println(len(pubKeyBytes))
-	fmt.Println(hex.EncodeToString(pubKeyBytes))
-	// rst := crypto.Keccak256Hash(bytes)
-	// fmt.Println(rst)
-	sig := make([]byte, crypto.SignatureLength)
-	fmt.Println(crypto.Ecrecover(pubKeyBytes, sig))
+	return ecies.ImportECDSA(key)
+}
+
+func main() {
+	priKey := hexKey("f8f8a2f43c8376ccb0871305060d7b27b0554d2cc72bccf41b2705608452f315")
+	pubkey := elliptic.Marshal(crypto.S256(), priKey.X, priKey.Y)
+	fmt.Println(hex.EncodeToString(pubkey))
 }
