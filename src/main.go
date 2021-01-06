187653a906e2efd5c49f526a8a5b44636daaac47
jimao <1982775886@qq.com>
2021-01-06 22:21:07 +0800

    实现私钥到地址的计算

--- src/main.go
+++ src/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"crypto/ecdsa"
 	"encoding/hex"
 	"fmt"
 
@@ -8,16 +9,21 @@ import (
 )
 
 func main() {
-	priKey, err := crypto.HexToECDSA("f8f8a2f43c8376ccb0871305060d7b27b0554d2cc72bccf41b2705608452f315")
+	priKeyHash := "796c823671b118258b53ef6056fd1f9fc96d125600f348f75f397b2000267fe8"
+	// 创建私钥对象
+	priKey, err := crypto.HexToECDSA(priKeyHash)
 	if err != nil {
 		panic(err)
 	}
 	priKeyBytes := crypto.FromECDSA(priKey)
-	fmt.Println(hex.EncodeToString(priKeyBytes))
-	pubKey := priKey.PublicKey
-	pubKeyBytes := crypto.FromECDSAPub(&pubKey)
-	fmt.Println(hex.EncodeToString(pubKeyBytes[1:]))
-	adrBytes := crypto.Keccak256(pubKeyBytes[1:])
-	fmt.Println(hex.EncodeToString(adrBytes[len(adrBytes)-20:]))
-	// 6e145ccef1033dea239875dd00dfb4fee6e3348b84985c92f103444683bae07b83b5c38e5e2b0c8529d7fa3f64d46daa1ece2d9ac14cab9477d042c84c32ccd0
+	fmt.Printf("私钥为: %s\n", hex.EncodeToString(priKeyBytes))
+
+	pubKey := priKey.Public().(*ecdsa.PublicKey)
+	// 获取公钥并去除头部0x04
+	pubKeyBytes := crypto.FromECDSAPub(pubKey)[1:]
+	fmt.Printf("公钥为: %s\n", hex.EncodeToString(pubKeyBytes))
+
+	// 获取地址
+	addr := crypto.PubkeyToAddress(*pubKey)
+	fmt.Printf("地址为: %s\n", addr.Hex())
 }
